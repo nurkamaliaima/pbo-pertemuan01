b46c131a59f201b059a75fbfa3181658f6cac092
nurkamaliaima <nurkamalia901@gmail.com>
2023-05-27 09:41:59 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
- public statis void main (String [] args){
-  System.out.println("Hello World !!");
+ public static void main (String [] args){
+  System.out.println("Hello Git !!");
+  System.out.println("Hello Java !!");
  }
 }
